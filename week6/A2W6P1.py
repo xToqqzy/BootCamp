17d81c5bdc3ece12ed80ffc3fff2135163799573
Dilshad Ataman <dilshad@145-137-27-102.wlan.hro.nl>
2024-10-15 15:37:48 +0200

    Made problem 1 of week 6

---
+++ week6/A2W6P1.py
@@ -0,0 +1,23 @@
+def unique_chars_dict(input_string):
+    char_count = {}
+
+    for char in input_string:
+        if char not in char_count:
+            char_count[char] = 1
+
+    return len(char_count)  # Return the number of unique characters
+
+
+def unique_chars_set(input_string):
+    unique_chars = set(input_string)
+    return len(unique_chars)
+
+
+if __name__ == "__main__":
+    user_input = input("Enter a string: ")
+
+    unique_count_dict = unique_chars_dict(user_input)
+    unique_count_set = unique_chars_set(user_input)
+
+    print(f"Unique characters (using dict): {unique_count_dict}")
+    print(f"Unique characters (using set): {unique_count_set}")
